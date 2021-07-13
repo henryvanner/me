4125e565ac0297dd7050c4790a41623d2aba7e60
henryvanner <henry.vanner@gmail.com>
2021-07-12 21:10:40 -0500

    Give classes better names

--- src/layouts/components/Header.tsx
+++ src/layouts/components/Header.tsx
@@ -2,43 +2,43 @@ import React, { useState } from 'react'
 import 'styles/Header.scss'
 
 const Header: React.FC = () => {
-  const [navBarIsOpen, setNavBarIsOpen] = useState(false)
-  const navBarClassName = navBarIsOpen ? 'navBar navBar--visible' : 'navBar'
+  const [navIsOpen, setNavIsOpen] = useState(false)
+  const navClassName = navIsOpen ? 'nav nav--visible' : 'nav'
 
   return (
     <header className='header'>
       <div className='appBar'>
-        <span className='appBar__toggler' onClick={ () => setNavBarIsOpen(true) }>
+        <span className='appBar__toggler' onClick={ () => setNavIsOpen(true) }>
           <i className='fas fa-bars fa-lg' />
         </span>
         <span className='appBar__title'></span>
       </div>
-      <div className={ navBarClassName }>
-        <div className='navBar__overlay' onClick={ () => setNavBarIsOpen(false) } />
-        <div className='navBar__content'>
-          <div className='navBarHeader'>
-            <h3 className='navBarHeader__name'>Henry Aquino</h3>
-            <span className='navBarHeader__email'>henry.vanner@gmail.com</span>
+      <div className={ navClassName }>
+        <div className='nav__overlay' onClick={ () => setNavIsOpen(false) } />
+        <div className='nav__content'>
+          <div className='navHeader'>
+            <h3 className='navHeader__name'>Henry Aquino</h3>
+            <span className='navHeader__email'>henry.vanner@gmail.com</span>
           </div>
           <nav>
-            <ul className='siteNavMenu'>
-              <li className='siteNavMenu__item siteNavMenu__item--selected'><a className='siteNavMenu__link' href='/my-life'>My Life</a></li>
-              <li className='siteNavMenu__item'><a className='siteNavMenu__link' href='/my-experience'>Experience</a></li>
-              <li className='siteNavMenu__item'><a className='siteNavMenu__link' href='/my-skills'>Skills</a></li>
+            <ul className='appNav'>
+              <li className='appNav__item appNav__item--selected'><a className='appNav__link' href='/my-life'>My Life</a></li>
+              <li className='appNav__item'><a className='appNav__link' href='/my-experience'>Experience</a></li>
+              <li className='appNav__item'><a className='appNav__link' href='/my-skills'>Skills</a></li>
             </ul>
           </nav>
           <nav>
-            <ul className='socialNavMenu'>
+            <ul className='socialNav'>
               <li>
-                <a className='socialNavMenu__link' href='https://github.com/henryvanner' rel='noreferrer' target='_blank'>
+                <a className='socialNav__link' href='https://github.com/henryvanner' rel='noreferrer' target='_blank'>
                   <i className='fab fa-github' />
-                  <span className='socialMediaName'>Github</span>
+                  <span className='socialNav__brandName'>Github</span>
                 </a>
               </li>
               <li>
-                <a className='socialNavMenu__link' href='https://www.linkedin.com/in/henryvanner/' rel='noreferrer' target='_blank'>
+                <a className='socialNav__link' href='https://www.linkedin.com/in/henryvanner/' rel='noreferrer' target='_blank'>
                   <i className='fab fa-linkedin-in' />
-                  <span className='socialMediaName'>LinkedIn</span>
+                  <span className='socialNav__brandName'>LinkedIn</span>
                 </a>
               </li>
             </ul>
