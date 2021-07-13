58de39a36b8f085d9491870806d6631dc5e00ceb
henryvanner <henry.vanner@gmail.com>
2021-07-12 22:10:08 -0500

    Introduce react-router

--- server/app.js
+++ server/app.js
@@ -4,25 +4,36 @@ const express = require('express')
 const app = express()
 
 const isDevMode = process.argv.some(val => val === '--dev')
+
 if (isDevMode) {
   const webpackDevServer = require('./webpackDevServer')
   webpackDevServer(app)
   console.log('server will be started in Dev mode')
-} else {
-  app.use(express.static('public'))
-  app.get('*', (_, res) => {
-      res.render('index.html')
-  })
 }
 
 /**
+ * Set this and installed ejs for `BrowserRouter` from `react-router`
+ * to work properly
+ */
+app.set('views', './public')
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'ejs')
+// EOC
+
+app.use(express.static('public'))
+app.get('*', (_, res) => {
+  res.render('index.html')
+})
+
+/**
  * This is this way so the app listens on the correct port
- * set by Heroku ( indicated by the env variable PORT )
+ * set by Heroku ( indicated by the `env` variable `PORT` )
  */
 let port = process.env.PORT
 if (port == null || port == '') {
   port = 3000;
 }
+// EOC
 
 app.listen(port, function () {
   console.log(`'me-haquino' is running on port ${port}!\n`)
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import Timeline from 'components/Timeline'
 import PageTitle from 'components/PageTitle'
-import myLifeEvents from './data/myLifeEvents.json'
 import MainLayout from 'layouts/MainLayout'
+import myLifeEvents from './data/myLifeEvents.json'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 const App: React.FC = () => {
   return (
-    <MainLayout>
-      <PageTitle>My Life</PageTitle>
-      <Timeline events={ myLifeEvents.data }/>
-    </MainLayout>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <MainLayout>
+            <PageTitle>My Life</PageTitle>
+            <Timeline events={ myLifeEvents.data }/>
+          </MainLayout>
+        </Route>
+        <Route exact path='/skills'>
+          <h1>Hey this is in development. We will let you know when we are done! :)</h1>
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
