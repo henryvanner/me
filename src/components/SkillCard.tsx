e3c320ec4e56502cadfd3028595f24b43f5467c2
henryvanner <henry.vanner@gmail.com>
2021-09-03 21:05:05 -0500

    Update Avatar to expect only sprintIndex prop

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -2,13 +2,10 @@ import React from 'react'
 import styled from 'styled-components'
 
 interface AvatarProps {
-  x: number,
-  y: number
+  spriteIndex: number
 }
 
-// spriteIndex is a 1-based index
 function getSpriteCoordinates (spriteIndex: number) {
-  // in pixels
   const spritesPadd = 2,
     spriteSize = 24,
     spritesPerRow = 4
@@ -16,18 +13,20 @@ function getSpriteCoordinates (spriteIndex: number) {
     colIndex = Math.ceil(spriteIndex / spritesPerRow) - 1
   const x = rowIndex * spriteSize + rowIndex * 2 * spritesPadd + 2,
     y = colIndex * spriteSize + colIndex * 2 * spritesPadd + 2
-  return [x, y]
+  return [-x, -y]
 }
 
 const Avatar = styled.div<AvatarProps>`
-  background-position: ${(props) => `${-props.x}px ${-props.y}px`} !important;
+  background-position: ${(props) => {
+    const [x, y] = getSpriteCoordinates(props.spriteIndex)
+    return `${x}px ${y}px`
+  }} !important;
 `
 
-const SkillCard: React.FC<Skill> = ({ spriteIndex = 1, name }) => {
-  const [x, y] = getSpriteCoordinates(spriteIndex)
+const SkillCard: React.FC<Skill> = ({ spriteIndex, name }) => {
   return (
     <div className='card card--isSkill'>
-      <Avatar className='card__avatarImage' x={ x } y={ y } />
+      <Avatar className='card__avatarImage' spriteIndex={ spriteIndex } />
       <span className='card__title'>{ name }</span>
     </div>
   )
